3782d4d1ead99a8eb17ac3165bc8586bf5367b82
bibizyano18 <artem.study18@gmail.com>
2026-03-10 20:44:54 +0300

    Добавил комментов в третий таск

--- tasks/task3-memoize.js
+++ tasks/task3-memoize.js
@@ -7,18 +7,23 @@
 */
 
 function memoize(fn) {
-  const cache = new Map();
-  return function (...args) {
-    const key = args.join(',');
-    if (cache.has(key)) {
-      console.log('cache key:', key);
-      return cache.get(key);
+    const cache = new Map(); // даем Мап для хранения кэша
+
+    return function (...args) {
+        const key = args.join(','); // достаём аргументы входящей функции, кладем их в строку
+        /* проверка, есть ли значения в кэше, если да,
+         то сразу достаем значения и не
+         тратим вычислительные мощности */
+        if (cache.has(key)) {
+            console.log('cache key:', key);
+            return cache.get(key);
+        }
+        // иначе просто считаем функцию и запоминаем ее результат
+        const result = fn(...args);
+        console.log('cache set:', key, result);
+        cache.set(key, result);
+        return result;
     }
-    const result = fn(...args);
-    console.log('cache set:', key, result);
-    cache.set(key, result);
-    return result;
-  }
 }
 
 const slowAdd = (a, b) => {
