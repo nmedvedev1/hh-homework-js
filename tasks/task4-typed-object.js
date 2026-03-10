14a947f15365719e136e59d9749391cc0e095863
bibizyano18 <artem.study18@gmail.com>
2026-03-10 20:32:26 +0300

    Выполнил таск 4

--- tasks/task4-typed-object.js
+++ tasks/task4-typed-object.js
@@ -7,14 +7,41 @@
 */
 
 function typedObject(schema) {
-  // TODO: реализуйте
+    const storage = {};
+
+    return new Proxy(storage, {
+        set(target, key, value) {
+            if (!(key in schema)) {
+                throw new Error(`Отсутствует ключ "${key}"`);
+            }
+
+            if (typeof value !== schema[key]) {
+                throw new Error(`Неверный тип данных`);
+            }
+            else {
+                target[key] = value;
+                return true;
+            }
+        },
+        get(target, key) {
+            return target[key];
+        }
+    });
 }
 
 const user = typedObject({
-  name: "string",
-  age: "number",
+    name: "string",
+    age: "number",
 });
 
-user.name = "Ivan"; // выполнится
-user.age = 20;      // выполнится
-user.age = "20";    // должно выбросить ошибку
+try {
+    user.name = "Ivan"; // выполнится
+    console.log(user.name); // "Ivan"
+
+    user.name = 20;      // выполнится
+    console.log(user.age); // 20
+
+    user.age = "20";    // должно выбросить ошибку
+} catch (error) {
+    console.error(error.message);
+}
