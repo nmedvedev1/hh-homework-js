158d23b4ecae6207bbe134242a3ea656835663fe
ksemu1 <145056072+ksemu1@users.noreply.github.com>
2026-03-17 00:56:18 +0300

    Update task3-memoize.js

--- tasks/task3-memoize.js
+++ tasks/task3-memoize.js
@@ -14,3 +14,29 @@ function memoize(fn) {
     return result;
   };
 }
+
+
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = args.join(",");
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+    cache.set(key, result);
+
+    return result;
+  };
+}
+
+const slowAdd = (a, b) => {
+  return a + b;
+};
+
+const memoAdd = memoize(slowAdd);
+memoAdd(1, 2); // возвращает 3
+memoAdd(1, 2); // из кэша, возвращает 3
