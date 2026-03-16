ac97e3a50a95f4796079d621297799cc4988dcf2
ksemu1 <145056072+ksemu1@users.noreply.github.com>
2026-03-17 00:33:49 +0300

    Update task3-memoize.js

--- tasks/task3-memoize.js
+++ tasks/task3-memoize.js
@@ -1,19 +1,16 @@
-/*
-Задание 3: Реализуйте memoize для функций
-
-Ограничения:
-- Аргументы функции — только строки или числа (для упрощения)
-- Кэшируйте результат по аргументам
-*/
-
 function memoize(fn) {
-  // TODO: реализуйте
+  const cache = new Map();
+
+  return function (...args) {
+    const key = args.join(",");
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+    cache.set(key, result);
+
+    return result;
+  };
 }
-
-const slowAdd = (a, b) => {
-  return a + b;
-};
-
-const memoAdd = memoize(slowAdd);
-memoAdd(1, 2); // возвращает 3
-memoAdd(1, 2); // из кэша, возвращает 3
