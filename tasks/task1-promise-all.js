219cb92a32e31bd1cc5f615011adb11d29e1917e
ksemu1 <145056072+ksemu1@users.noreply.github.com>
2026-03-17 00:32:23 +0300

    Update task1-promise-all.js

--- tasks/task1-promise-all.js
+++ tasks/task1-promise-all.js
@@ -1,17 +1,19 @@
-/*
-Задание 1: Реализуйте свой Promise.all
-
-Требования:
-- Принимает список промисов
-- Резолвится массивом результатов в том же порядке
-- Немедленно реджектится при первой ошибке
-*/
-
 function promiseAll(promises) {
-  // TODO: реализуйте
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completed = 0;
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          results[index] = value;
+          completed++;
+
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    });
+  });
 }
-
-const p1 = Promise.resolve(1);
-const p2 = Promise.resolve(2);
-
-promiseAll([p1, p2]).then(console.log); // [1, 2]
