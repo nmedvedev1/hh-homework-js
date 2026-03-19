9c969a6f5caf36c27c99ea87931bc394caf871a0
Глеб Сычев <gleb.asychev@gmail.com>
2026-03-19 22:44:06 +0300

    Task 1 commit

--- tasks/task1-promise-all.js
+++ tasks/task1-promise-all.js
@@ -8,10 +8,36 @@
 */
 
 function promiseAll(promises) {
-  // TODO: реализуйте
+  return new Promise((resolve, reject) => {
+    let results = new Array(promises.length);
+    let count = 0;
+
+    if (promises.length === 0) {
+      resolve([]);
+      return;
+    }
+    for (let i = 0; i < promises.length; i++) {
+      let promise = promises[i];
+
+      Promise.resolve(promise)
+        .then((value) => {
+          results[i] = value;
+          count++;
+
+          if (count === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
+  });
 }
 
 const p1 = Promise.resolve(1);
 const p2 = Promise.resolve(2);
 
 promiseAll([p1, p2]).then(console.log); // [1, 2]
+
+promiseAll([]).then(console.log);
